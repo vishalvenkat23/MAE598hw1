5cbd57f12c71f6601b3f671fba350d0bc85554eb
vishalvenkat23 <vvenka52@asu.edu>
2021-08-23 22:51:43 -0700

    V1.4 corrected mistakes

--- Homework/HW_1_solution_1.py
+++ Homework/HW_1_solution_1.py
@@ -2,5 +2,5 @@
 
 # trying to check if the changes show up on git
 
-# I assume i got the hang of using Git by now but should see if what i am goind is
-# right in next class
+# I assume i got the hang of using Git by now but should see if what
+# i am doing is right in next class
