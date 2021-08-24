0ce3828282fb1226c31a20783c680682193ad127
vishalvenkat23 <vvenka52@asu.edu>
2021-08-23 22:50:24 -0700

    V1.3

--- Homework/HW_1_solution_1.py
+++ Homework/HW_1_solution_1.py
@@ -1,3 +1,6 @@
 # checking if the git works
 
 # trying to check if the changes show up on git
+
+# I assume i got the hang of using Git by now but should see if what i am goind is
+# right in next class
