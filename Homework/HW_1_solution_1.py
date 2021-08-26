4e0124dd4cdb3f75ef72e7c92debe0c649b41f78
vishalvenkat23 <vvenka52@asu.edu>
2021-08-25 22:58:52 -0700

    V1.8 solved example HW1

--- Homework/HW_1_solution_1.py
+++ Homework/HW_1_solution_1.py
@@ -4,4 +4,11 @@ from scipy.optimize import minimize
 
 fun = lambda x: (x[0]-x[1])**2 + (x[1] + x[2] - 2)**2 + (x[3] - 1)**2 + (x[4] - 1)**2
 
-cons = ({ })
+cons = ({'type': 'eq', 'fun': lambda x:  x[0] + 3 * x[1]},
+        {'type': 'eq', 'fun': lambda x: -x[2] + x[3] - 2 * x[4]},
+        {'type': 'eq', 'fun': lambda x: -x[1] - x[4]})
+
+bnds = ((-10, 10), (-10, 10), (-10, 10), (-10, 10), (-10, 10))
+res = minimize(fun, (1,2,3,4,5), method='SLSQP', bounds=bnds, constraints=cons)
+
+print(res)
